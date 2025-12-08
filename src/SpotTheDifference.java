d179d06d0fd184b1b381c9509b6058695f1b7d1b
nourhandardeer <nourhandardeer3@gmail.com>
2025-12-08 21:33:19 +0200

    update

---
+++ src/SpotTheDifference.java
@@ -0,0 +1,64 @@
+package game;
+
+import javax.media.opengl.*;
+import javax.media.opengl.glu.GLU;
+import com.sun.opengl.util.GLUT;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.net.URL;
+import java.nio.ByteBuffer;
+import java.util.*;
+import java.util.List;
+import javax.sound.sampled.*;
+
+import com.sun.opengl.util.j2d.TextRenderer;
+import game.texture.TextureReader;
+
+public class SpotTheDifference implements GLEventListener, MouseListener {
+
+
+    @Override
+    public void init(GLAutoDrawable glAutoDrawable) {
+
+    }
+
+    @Override
+    public void display(GLAutoDrawable glAutoDrawable) {
+
+    }
+
+    @Override
+    public void reshape(GLAutoDrawable glAutoDrawable, int i, int i1, int i2, int i3) {
+
+    }
+
+    @Override
+    public void displayChanged(GLAutoDrawable glAutoDrawable, boolean b, boolean b1) {
+
+    }
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+}
