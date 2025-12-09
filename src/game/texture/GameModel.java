91e34ac8f3d23b3996d44a65ab8a39203db10ee3
Sarah Abdelazim <abdelazimsarah4@gmail.com>
2025-12-09 14:31:07 +0200

    update

--- src/game/texture/GameModel.java
+++ src/game/texture/GameModel.java
@@ -10,4 +10,21 @@ import javax.swing.*;
 import javax.swing.Timer;
 
 public class GameModel {
+    public String username;
+    public int level;
+    public int score = 0;
+    public int lives = 3;
+    public int timeLeft; // seconds
+    public List<Point> differences; // إحداثيات الاختلاف الأصلية
+    private GameController controller;
+
+    private Timer timer;
+    private boolean showMessage = false;
+    private String messageText = "";
+    private float messageAlpha = 0f;
+    private int messageY = -50; // يبدأ من فوق الشاشة
+    private final int targetY = 350;
+    private Clip winClip;
+    private Clip loseClip;
+    private List<Point> foundPoints = new ArrayList<>();
 }
